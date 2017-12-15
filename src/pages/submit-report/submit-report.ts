5d4c757e666059c47c35d652445f87a14bbff7c4
elishaking <ek.chibueze@gmail.com>
2017-12-15 12:43:05 +0100

    added submit-report-page

---
+++ src/pages/submit-report/submit-report.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-submit-report',
+  templateUrl: 'submit-report.html',
+})
+export class SubmitReportPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    // console.log('ionViewDidLoad SubmitReportPage');
+  }
+
+}
