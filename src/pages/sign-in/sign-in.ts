846d9a5c41173077e2210a9feea6f2c8ded4d26f
elishaking <ek.chibueze@gmail.com>
2017-12-15 14:02:33 +0100

    updated sign-in page

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
+
 @Component({
   selector: 'page-sign-in',
   templateUrl: 'sign-in.html',
@@ -14,4 +16,7 @@ export class SignInPage {
     // console.log('ionViewDidLoad SignInPage');
   }
 
+  signIn(){
+    this.navCtrl.setRoot(HomePage);
+  }
 }
