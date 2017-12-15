9383aef0dadbb991947bdba18594f945779d2cb4
elishaking <ek.chibueze@gmail.com>
2017-12-15 15:46:01 +0100

    updated submit report

--- src/pages/submit-report/submit-report.ts
+++ src/pages/submit-report/submit-report.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AddReportFilePage } from '../add-report-file/add-report-file';
+
 @Component({
   selector: 'page-submit-report',
   templateUrl: 'submit-report.html',
 })
 export class SubmitReportPage {
 
+  nFiles = 0;
+  name = "Sign In";
+  signedIn = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad SubmitReportPage');
+  }
+
+  addNewFile(){
+    this.navCtrl.push(AddReportFilePage);
+  }
+
+  submitAsUser(){
+
+  }
+
+  submitAsUnknown(){
+
   }
 
 }
