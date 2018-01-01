5f4e30f840b1501074b0433069d4ba37c0bd5f84
elishaking <ek.chibueze@gmail.com>
2018-01-01 11:25:11 +0100

    updated config

--- src/pages/submit-report/submit-report.ts
+++ src/pages/submit-report/submit-report.ts
@@ -38,6 +38,11 @@ export class SubmitReportPage {
     this.headers = new Headers();
     this.headers.append('Authorization', 'JWT ' + this.navParams.get('token'));
     // console.log('ionViewDidLoad SubmitReportPage');
+    let media = document.querySelector('input');
+    media.addEventListener('change', (ev)=>{
+      console.log(ev);
+      console.log(media.files);
+    })
   }
 
   // addNewFile(){
@@ -185,15 +190,21 @@ export class SubmitReportPage {
       { headers: this.headers }
     ).subscribe((res) => {
       console.log('report created');
-      let report = res.json();
-      this.http.post(
-        'http://localhost:8000/api/media/create/',
-        {
-          // file: new File(),//this.media,
-          report: report.id | 1
-        },
-        { headers: this.headers }
-      )
+      // let report = res.json();
+      // console.log('report', report);
+      // let media = <HTMLInputElement>document.getElementById('media');
+      // // media.value = "C:\\Users\\KING\\Pictures\\IMG_20170122_111810.jpg"; //this.media;
+      // console.log(media);
+      // let mediaFile = media.files[0];
+      // console.log(mediaFile);
+      // this.http.post(
+      //   'http://localhost:8000/api/media/create/',
+      //   {
+      //     file: media,//this.media,
+      //     report: report.id | 1
+      //   },
+      //   { headers: this.headers }
+      // )
       // for(let i = 0; i < this.nFiles; i++){
       //   this.http.post(
       //     '',
